05301026586fddec592b4b679f4a79a6b53b8451
Winfred Huang <huang.winfred21@gmail.com>
2022-01-07 11:56:42 -0800

    Add a useState hook

--- react-hooks-context-migration/react-redux-app-starter/src/containers/header.js
+++ react-hooks-context-migration/react-redux-app-starter/src/containers/header.js
@@ -26,6 +26,9 @@ class Header extends Component {
           <Link to='/renderlist' style={{padding: '5px'}}>
              List
           </Link>
+          <Link to='/hookscontainer' style={{padding: '5px'}}>
+             Hooks Container
+          </Link>
           <Link to='/privateroute' style={{padding: '5px'}}>
             Private Route
           </Link>
--- react-hooks-context-migration/react-redux-app-starter/src/hooks/hooks_container1.js
+++ react-hooks-context-migration/react-redux-app-starter/src/hooks/hooks_container1.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 //main app 
 const HooksContainer1 = () => {
 
-  return(
-    <div>
-      React Hooks
-    </div>
-  )
+    const [ stateValue, setValue ] = useState(0);
+
+    const incrementValue = () => {
+        setValue(stateValue + 1);
+    }
+
+    const decrementValue = () => {
+        setValue(stateValue - 1);
+    }
+
+    return(
+        <div>
+        React Hooks
+        <br />
+
+        <button onClick={() => incrementValue()}>Increase local state</button>
+        <button onClick={() => decrementValue()}>Decrease local state</button>
+
+        <br />
+
+        <div>
+            <p>Local State: {stateValue}</p>
+        </div>
+
+        </div>
+    )
 }
 
 
