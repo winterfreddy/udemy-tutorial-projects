452a3d030ef9d2106bd63f749a6b94effdc963a0
Winfred Huang <huang.winfred21@gmail.com>
2022-01-07 22:10:12 -0800

    Add useEffect hook

--- react-hooks-context-migration/react-redux-app-starter/src/hooks/hooks_container1.js
+++ react-hooks-context-migration/react-redux-app-starter/src/hooks/hooks_container1.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 //main app 
 const HooksContainer1 = () => {
 
     const [ stateValue, setValue ] = useState(0);
+    const [ useEffectValue, setUseEffectValue ] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => setUseEffectValue("useEffect worked"), 3000);
+    }, [stateValue])
 
     const incrementValue = () => {
         setValue(stateValue + 1);
@@ -14,6 +19,10 @@ const HooksContainer1 = () => {
         setValue(stateValue - 1);
     }
 
+    const changeEffect = () => {
+        setUseEffectValue("some string");
+    }
+
     return(
         <div>
         React Hooks
@@ -21,10 +30,14 @@ const HooksContainer1 = () => {
 
         <button onClick={() => incrementValue()}>Increase local state</button>
         <button onClick={() => decrementValue()}>Decrease local state</button>
+        <button onClick={() => changeEffect()}>Change Effect Value</button>
 
         <br />
 
         <div>
+            <br />
+            { useEffectValue ? <p>{useEffectValue}</p> : <p>No value</p>}
+            <br />
             <p>Local State: {stateValue}</p>
         </div>
 
