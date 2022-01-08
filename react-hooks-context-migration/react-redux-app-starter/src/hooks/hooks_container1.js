04b198f567f30f9b66af32bbedfdf19211c33c90
Winfred Huang <huang.winfred21@gmail.com>
2022-01-07 22:24:18 -0800

    Add useReducer hook

--- react-hooks-context-migration/react-redux-app-starter/src/hooks/hooks_container1.js
+++ react-hooks-context-migration/react-redux-app-starter/src/hooks/hooks_container1.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useReducer } from 'react';
+import * as Reducer from '../store/hooks_state/hooks_reducer';
+import * as ACTIONS from '../store/actions/actions';
 
 //main app 
 const HooksContainer1 = () => {
 
     const [ stateValue, setValue ] = useState(0);
     const [ useEffectValue, setUseEffectValue ] = useState(null);
+
+    const [ state, dispatch ] = useReducer(Reducer.HooksReducer, Reducer.initialState);
 
     useEffect(() => {
         setTimeout(() => setUseEffectValue("useEffect worked"), 3000);
@@ -23,6 +26,14 @@ const HooksContainer1 = () => {
         setUseEffectValue("some string");
     }
 
+    const handleDispatchTrue = () => {
+        dispatch(ACTIONS.success());
+    }
+    
+    const handleDispatchFalse = () => {
+        dispatch(ACTIONS.failure());
+    }
+
     return(
         <div>
         React Hooks
@@ -31,12 +42,16 @@ const HooksContainer1 = () => {
         <button onClick={() => incrementValue()}>Increase local state</button>
         <button onClick={() => decrementValue()}>Decrease local state</button>
         <button onClick={() => changeEffect()}>Change Effect Value</button>
+        <button onClick={() => handleDispatchTrue()}>Dispatch True</button>
+        <button onClick={() => handleDispatchFalse()}>Dispatch False</button>
 
         <br />
 
         <div>
             <br />
             { useEffectValue ? <p>{useEffectValue}</p> : <p>No value</p>}
+            <br />
+            { state.stateprop1 ? <p>State prop 1 is true</p> : <p>State prop 1 is false</p>}
             <br />
             <p>Local State: {stateValue}</p>
         </div>
